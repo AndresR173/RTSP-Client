0cec917daaa8ef312e5c342da570e70c54e98dab
Andres Rojas <afro17@gmail.com>
2016-05-18 10:56:22 -0500

    Initial Commit

---
+++ RtspClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RtspClient
+//
+//  Created by Andres Rojas on 18/05/16.
+//  Copyright © 2016 Andres Rojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
